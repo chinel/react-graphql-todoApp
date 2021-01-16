ca939010a24c2acb08f852c9c55bf413db53181d
nelo <nelokwaz@gmail.com>
2021-01-16 21:11:44 +0100

    get the todo list from the graphql api using useQuery hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
+const GET_TODOS = gql`
+  query getTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+`;
 //list todos
 //add todo
 //toggle todos
 //delete todos
 
 function App() {
+  const { data, loading } = useQuery(GET_TODOS);
+
   return <div>App</div>;
 }
 
