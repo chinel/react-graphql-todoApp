4ef43128129cdc8e833716b7e26e62934d1d4f3e
nelo <nelokwaz@gmail.com>
2021-01-16 20:54:50 +0100

    cleaned up the index component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { ApolloClient, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://react-todo-graphql-api-1.herokuapp.com/v1/graphql",
+  cache: new InMemoryCache(),
 });
-
-client.query({});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
