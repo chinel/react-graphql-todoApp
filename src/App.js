cb6df94441336f79a40040ac05503f925d5a1429
nelo <nelokwaz@gmail.com>
2021-01-17 08:31:13 +0100

    styled the application

--- src/App.js
+++ src/App.js
@@ -20,19 +20,32 @@ function App() {
   if (loading) return <div>Loading todos</div>;
   if (error) return <div>Error fetching todos</div>;
   return (
-    <div>
-      <h1>GraphQL Checklist</h1>
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1">
+      <h1 className="f2-l">
+        GraphQL Checklist{" "}
+        <span role="img" aria-label="Checkmark">
+          ✅
+        </span>
+      </h1>
       {/* Todo Form */}
-      <form action="">
-        <input type="text" placeholder="Write your todo" />
-        <button type="submit">Create</button>
+      <form className="mb3">
+        <input
+          className="pa2 f4 b--dashed"
+          type="text"
+          placeholder="Write your todo"
+        />
+        <button className="pa2 f4 bg-green" type="submit">
+          Create
+        </button>
       </form>
       {/* Todo List*/}
-      <div>
+      <div className="flex items-center justify-center flex-column">
         {data.todos.map((todo) => (
           <p key={todo.id}>
-            <span>{todo.text}</span>
-            <button>&times;</button>
+            <span className="pointer list pa1 f3">{todo.text}</span>
+            <button className="bg-transparent bn f4">
+              <span className="red">&times;</span>
+            </button>
           </p>
         ))}
       </div>
